31a3bb5bc0d25b9cdb187472f1b867b075ff56c5
Nick Bourque <W0290474@nscc.ca>
2017-02-03 13:12:57 -0400

    Updated XML comments after grading.

--- NickBourque_PROG2200_Assignment2/ChatLibrary/Delegates.cs
+++ NickBourque_PROG2200_Assignment2/ChatLibrary/Delegates.cs
@@ -1,5 +1,10 @@
 ﻿
 namespace ChatLibrary
 {
+    /// <summary>
+    /// Handles the event of receiving a message.
+    /// </summary>
+    /// <param name="sender">Client object</param>
+    /// <param name="e">An incoming message</param>
     public delegate void MessageReceivedEventHandler(object sender, MessageReceivedEventArgs e);
 }
