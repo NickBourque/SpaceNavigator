8ff19174f1962d6f3518eda072a532099f164008
W0290474 <W0290474@nscc.ca>
2017-03-27 15:50:50 -0300

    Deleted everything except SpaceNavigator

--- TaskExecuterSample/TasksLib/Delegates.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace TasksLib
-{
-    public delegate void ProgressChangedEventHandler(object sender, ProgressChangedEventArgs e);
-
-    public delegate void ProgressCompleteEventHandler(object sender, EventArgs e);
-}
